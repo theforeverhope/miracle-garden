4d1a1a0ff4f44b0a5d806bba7615bb7d9a203e01
theforeverhope <theforeverhope@163.com>
2023-01-26 23:44:52 +0800

    feat: combine watermelon game

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <router-link to="/cafeteria"><img src="../../assets/home/cafeteria.png" class="home-cafeteria"/></router-link>
-    <router-link to="/watermelon"><img src="../../assets/home/fruits.png" class="home-watermelon"/></router-link>
+    <router-link to="/fruit"><img src="../../assets/home/fruits.png" class="home-fruit"/></router-link>
   </div>
 </template>
 
